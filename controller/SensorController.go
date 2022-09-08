f44f44fd85ea33ff789f0f44eb0bd5d4939b7bc0
salmonfishy <1564309204@qq.com>
2022-09-08 11:04:05 +0800

    api-sensor page

--- controller/SensorController.go
+++ controller/SensorController.go
@@ -12,28 +12,42 @@ func GetSensorInfo(ctx *gin.Context) {
 	db := common.GetDB()
 
 	//获取前端传入的参数
-	json := make(map[string]interface{}) //注意该结构接受的内容
-	ctx.ShouldBind(&json)
-
-	if _, ok := json["DeviceID"]; !ok {
-		ctx.JSON(http.StatusUnprocessableEntity, gin.H{"code": http.StatusUnprocessableEntity, "msg": "传入的值有误"})
+	type JSON struct {
+		DeviceID uint
+		ApiTag   string
+		Page     int
+		PageSize int
+	}
+	var json JSON
+	err := ctx.BindJSON(&json)
+	if err != nil {
+		ctx.JSON(http.StatusBadRequest, gin.H{"code": http.StatusBadRequest, "msg": "传入数据有误"})
 		return
 	}
 
 	//查询数据，返回结果
+	offset := (json.Page - 1) * json.PageSize
+	var count int64
 	var sensors []model.Sensor
-	if _, ok := json["ApiTag"]; !ok {
-		//1.不传入apiTag
-		db.Where("device_id = ?", json["DeviceID"]).Find(&sensors)
+	if len(json.ApiTag) == 0 {
+		//1.apiTag为空字符串
+		err := db.Where("device_id = ?", json.DeviceID).Limit(json.PageSize).Offset(offset).Find(&sensors).Limit(-1).Offset(-1).Count(&count).Error
+		if err != nil {
+			panic(err)
+		}
 	} else {
 		//2.传入apiTag
-		db.Where("device_id = ? AND api_tag = ? ", json["DeviceID"], json["ApiTag"]).First(&sensors)
+		err := db.Where("device_id = ? AND api_tag = ? ", json.DeviceID, json.ApiTag).First(&sensors).Count(&count).Error
+		if err != nil {
+			panic(err)
+		}
 	}
 
 	ctx.JSON(http.StatusOK, gin.H{
-		"code": http.StatusOK,
-		"msg":  "查询成功",
-		"data": sensors,
+		"code":  http.StatusOK,
+		"msg":   "查询成功",
+		"data":  sensors,
+		"total": count,
 	})
 }
 
